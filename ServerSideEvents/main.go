c5332643b60b7374dd256c9b12c1f97ca523501e
vatzmehta <vatsalmehta121297@gmail.com>
2026-04-13 05:22:36 +0000

    Adds ServerSideEvents

---
+++ ServerSideEvents/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+//go:embed index.html
+var indexHTML []byte
+
+func main() {
+	http.HandleFunc("/events", sseHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(indexHTML)
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("unable to start server %s", err.Error())
+	}
+
+}
+
+func sseHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	rc := http.NewResponseController(w)
+
+	clientGone := r.Context().Done()
+
+	t := time.NewTicker(time.Second)
+	var count int = 0
+	defer t.Stop()
+
+	for {
+		select {
+		case <-clientGone:
+			fmt.Println("client has disconnected")
+			return
+		case <-t.C:
+			if _, err := fmt.Fprintf(w, "event:test\ndata:Streaming Event %d\n\n", count); err != nil {
+				log.Printf("error in writing")
+			}
+			rc.Flush()
+			count++
+		}
+	}
+
+}
